2e8844eed8e23ffb02c4f8f2b972d136443263b3
Damai Subimawanto <damai.subimawanto@gmail.com>
2023-10-02 18:51:23 +0700

    Added layouts for main and add new city pages.

--- app/src/main/java/com/damai/deloitteweather/ui/main/MainActivity.kt
+++ app/src/main/java/com/damai/deloitteweather/ui/main/MainActivity.kt
@@ -1,7 +1,29 @@
 package com.damai.deloitteweather.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import com.damai.base.BaseActivity
+import com.damai.deloitteweather.R
+import com.damai.deloitteweather.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
+    override val layoutResource: Int = R.layout.activity_main
+
+    override val viewModel: MainViewModel by viewModel()
+
+    override fun ActivityMainBinding.viewInitialization() {
+        TODO("Not yet implemented")
+    }
+
+    override fun ActivityMainBinding.setupListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun ActivityMainBinding.setupObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun ActivityMainBinding.onPreparationFinished() {
+        TODO("Not yet implemented")
+    }
 }
---
+++ app/src/main/java/com/damai/deloitteweather/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.damai.deloitteweather.ui.main
+
+import android.app.Application
+import com.damai.base.BaseViewModel
+import com.damai.base.coroutines.DispatcherProvider
+
+/**
+ * Created by damai007 on 02/October/2023
+ */
+class MainViewModel(
+    app: Application,
+    private val dispatcher: DispatcherProvider
+) : BaseViewModel(app = app) {
+
+
+}
